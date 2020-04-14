1bb3c72f03b19adcbd067004361d4cc6904c6d10
Michael A. Agarenzo <michaelagarenzo@mail.adelphi.edu>
2020-04-14 02:40:57 -0400

    Restart project, clean up folder structure

--- projectCarousel.js
+++ projectCarousel.js
@@ -1,53 +1,15 @@
-// const profileName = "magarenzo";
+/**
+ * Scrape data and loop through pinned repositories listed on a GitHub user's profile
+ * @param {String} id
+ */
 
-document.getElementById("projects").innerHTML = "<p>test</p>";
+function projectCarousel(id) {
 
-// const githubScraper = require("github-scraper");
-// const profile = "magarenzo";
+    $.ajax({
+        type: "Post",
+        url: "/test.py"
+    }).done(function() {
+        document.getElementById(id).innerHTML = "<p>Done executing test.py</p>";
+    });
 
-require(["github-scraper"], function() {
-    alert("done");
-});
-
-// githubScraper(profile, function(err, profileData) {
-//     alert(data);
-// });
-
-// require(["github-scraper"], function() {
-//     console.log("in require");
-//     testFunction();
-// });
-
-// function testFunction() {
-//     console.log("in testFunction");
-// }
-
-// // grab profileData of user specified in profileName
-// require(["github-scraper"], function() {
-
-//     githubScraper(profileName, function(err, profileData) {
-
-//         var repo;
-//         var repos = [];
-
-//         if (err) throw new Error("githubScraper error on profileData: " + err);
-//         else {
-//             // if profileData has object we need, grab each repo's name
-//             if (profileData.pinned) {
-//                 Object.keys(profileData.pinned).forEach(function(key) {
-//                     repo = JSON.stringify(profileData.pinned[key]);
-//                     repoName = repo.substring(repo.lastIndexOf(profileName + "/"), repo.lastIndexOf('"}'));
-
-//                     // use repoName to get each repo's data
-//                     githubScraper(repoName, function(err, repoData) {
-//                         if (err) throw new Error("githubScraper error on repoName: " + err);
-//                         else {
-//                             // store repoData in repos array
-//                             repos.push(JSON.stringify(repoData, null, 2));
-//                         }
-//                     });
-//                 });
-//             }
-//         }
-//     })
-// });
+}
