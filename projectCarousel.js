92730478b1b088fcc47ebaf8371501e42f09a59c
Michael A. Agarenzo <michaelagarenzo@mail.adelphi.edu>
2020-04-20 01:19:21 -0400

    Working with GitHub API

--- projectCarousel.js
+++ projectCarousel.js
@@ -1,10 +1,28 @@
 /**
- * Scrape data and loop through pinned repositories listed on a GitHub user's profile
+ * Fetch data and display repositories listed on a GitHub user's profile
  * @param {String} id
  */
 
 function projectCarousel(id) {
 
-    document.getElementById(id).innerHTML = "<p>test</p>"
+    const profileName = "magarenzo";
+    const div = document.getElementById(id);
+
+    fetch("https://api.github.com/users/" + profileName + "/repos").then((response) => {
+
+        return response.json()
+
+    }).then((jsonObject) => {
+
+        console.log(jsonObject)
+
+        for (var key in jsonObject) {
+            div.innerHTML += "<p>";
+            var name = JSON.stringify(jsonObject[key].name).split('"').join("");
+            div.innerHTML += name;
+            div.innerHTML += "</p><br>";
+        }
+
+    });
 
 }
